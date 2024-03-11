95bd675b27c01e33a3eee0681c5051daac26c102
Ahmad Omer <OMRAHM010@myuct.ac.za>
2024-03-11 10:50:43 +0200

    Added seeddata

---
+++ Workshop 1/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Workshop_1.Data;
+
+namespace Workshop_1.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new Workshop_1Context(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<Workshop_1Context>>()))
+        {
+            if (context == null || context.Movies == null)
+            {
+                throw new ArgumentNullException("Null Workshop_1Context");
+            }
+
+            // Look for any Movies.
+            if (context.Movies.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movies.AddRange(
+                new Movies
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+
+                new Movies
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+
+                new Movies
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                },
+
+                new Movies
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
